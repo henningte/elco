4921e839caa102082ab99cb6745f0830710af365
henningte <34153309+henningte@users.noreply.github.com>
2020-09-29 10:21:42 +0200

    Updated the isotope signature correction procedure: Only one pre-defined standard is used for the correction. Corrected signatures of additional standards can be checked with the respective reference values and pre-defined threshold values.

--- R/elco_irms_correct_isotopes.R
+++ R/elco_irms_correct_isotopes.R
@@ -6,15 +6,17 @@
 #' sample is located in.
 #' The procedure is as follows:
 #' \enumerate{
-#'   \item For each standard, compute the median isotope signature and median signal area for the respective isotope.
-#'   The median is used instead of an average to prevent potential outliers from having too large an effect.
-#'   \item For each standard, record the number of measurements. This is used to exclude using standards
-#'   as reference with too few measurmements since these will have large uncertainties in their estimated
-#'   median. From the list of standards, discard all with <t measurements, where t is an integer value >1.
-#'   \item For each sample (including the measured standards), compute the absolute difference in the signal area for the respective isotope to all
-#'   standards' median signal area values. Record for each sample the standard with the least absolute difference.
-#'   \item Look up the known reference isotope signatures for each assigned standard and substract this value
-#'   from the measured value.
+#'   \item For a standard defined by the user, compute the median of the individually measured isotope signals
+#'   for the selected isotope. The defined standard should be a trusted standard.
+#'   \item For the same standard, count the number of samples that have been measured. If this number is smaller
+#'   than a threshold \code{t}, print a warning. This is done to avoid that correction is done without enough
+#'   samples to reliably compute a median value.
+#'   \item Look up the known reference isotope signature for the selected standard. Compute the difference between
+#'   this value and the median of the isotope signature of the standard. Take this difference and substract it
+#'   from each measured value.
+#'   \item (Optionally): Extract the values of other standards after the correction, compute their medians, and
+#'   check if the absolute deviation from the reference value is larger than some threshold. Print a warning
+#'   if this is the case.
 #'   \item (Optionally): create a plot showing uncorrected and corrected isotope signatures for all measured
 #'   standards along their known reference values. This plot may be used as a visual check of the heuristic
 #'   correction procedure.
@@ -23,6 +25,13 @@
 #' @param x An object of class \code{\link[elco:elco_new_irms]{irms}}.
 #' @param isotope A character value representing the isotope for which to correct isotope signatures.
 #' This must be one of "13C" or "15N".
+#' @param ref A \code{data.frame} with the same format as \code{\link[elco:irms_standards]{irms_standards}}
+#' and exactly one row. \code{ref} contains the data for the standard to use for the correction.
+#' @param check A \code{data.frame} with the same format as \code{\link[elco:irms_standards]{irms_standards}}.
+#' \code{check} contains data for standards with which to check the correction. If the median of their
+#' corrected isotope signature values deviates (absolutely) by more than a threshold value as defined by
+#' \code{check$threshold_13C} or \code{check$threshold_15N} (depending on the value of \code{isotope}), a
+#' warning will be printed. Can alternatively be set to \code{NULL}, in which case no warning is printed.
 #' @param t An integer value specifying the number of measurements that must be available for a particular
 #' standard to make it eligble for being a reference for correcting the measured isotope values.
 #' @param by_file A logical value indicating if medians of standards are computed across different files
@@ -35,10 +44,12 @@
 #' @return nothig.
 #' @export
 elco_irms_correct_isotopes <- function(x,
-                                      isotope = "13C",
-                                      t = 5,
-                                      by_file = TRUE,
-                                      plotit = FALSE) {
+                                       ref = irms_standards[irms_standards$standard_name == "BBOT_CN.", ],
+                                       check = irms_standards,
+                                       isotope = "13C",
+                                       t = 5,
+                                       by_file = TRUE,
+                                       plotit = FALSE) {
 
   # checks
   elco_check_irms(x)
@@ -52,8 +63,37 @@ elco_irms_correct_isotopes <- function(x,
   if(!isotope %in% c("13C", "15N")) {
     rlang::abort(paste0("`isotope` must be one of c('13C', '15N'), but is ", isotope, "."))
   }
+  if(!is.data.frame(ref)) {
+    rlang::abort(paste0("`ref` must be a data.frame, but is of class ", class(ref)[[1]], "."))
+  }
+  if(nrow(ref) != 1) {
+    rlang::abort(paste0("`ref` must have exactly one row, but has ", nrow(ref), " rows."))
+  }
+  data("irms_standards", package = "elco")
+  irms_standards_colnames <- colnames(irms_standards)
+  cond <- !colnames(ref) %in% irms_standards_colnames
+  if(any(cond)) {
+    if(sum(cond) == 1) {
+      rlang::abort(paste0('`ref` must contain the columns ', paste(irms_standards_colnames, collapse = ", "), ', but contains a mismatching column ', colnames(ref)[cond], '.'))
+    } else {
+      rlang::abort(paste0('`ref` must contain the columns ', paste(irms_standards_colnames, collapse = ", "), ', but contains the mismatching columns ', paste(colnames(ref)[cond], collapse = ", "), '.'))
+    }
+  }
+  if(!(is.data.frame(check) | is.null(check))) {
+    rlang::abort(paste0("`check` must be a data.frame or `NULL`, but is of class ", class(check)[[1]], "."))
+  }
+  if(is.data.frame(check)) {
+    cond <- !colnames(check) %in% irms_standards_colnames
+    if(any(cond)) {
+      if(sum(cond) == 1) {
+        rlang::abort(paste0('`check` must contain the columns ', paste(irms_standards_colnames, collapse = ", "), ', but contains a mismatching column ', colnames(ref)[cond], '.'))
+      } else {
+        rlang::abort(paste0('`check` must contain the columns ', paste(irms_standards_colnames, collapse = ", "), ', but contains the mismatching columns ', paste(colnames(ref)[cond], collapse = ", "), '.'))
+      }
+    }
+  }
   if(!is.numeric(t)) {
-    rlang::abort(paste0("`isotope` must be a numeric value, but is of class ", class(t)[[1]], "."))
+    rlang::abort(paste0("`t` must be a numeric value, but is of class ", class(t)[[1]], "."))
   }
   cond <- length(t)
   if(cond != 1) {
@@ -88,12 +128,17 @@ elco_irms_correct_isotopes <- function(x,
   }
 
   # get data on standards
-  data("irms_standards", package = "elco")
-  irms_standards_sel <- irms_standards[, c("standard_name", as.character(disotope))]
+  irms_standards_sel <- ref[, c("standard_name", as.character(disotope))]
   colnames(irms_standards_sel) <- c("sample_label", "disotope")
 
-  # get median isotope signature and isotope signal area and the number of measurements for each standard
+  # new column names for `check` to facilitate joining
+  if(!is.null(check)) colnames(check)[[1]] <- "sample_label"
+
+  # get median isotope signature and isotope signal area and the number of measurements for the reference standard
   x_standards <- purrr::map(x, elco_irms_extract_standards)
+  x_standards <- purrr::map(x_standards, function(y) {
+    y[y$sample_label == irms_standards_sel$sample_label, ]
+  })
   x_standards_signature_median <- purrr::map(x_standards, function(y) {
     y <- dplyr::group_by(y, sample_label)
     y <- dplyr::summarise(y,
@@ -107,66 +152,79 @@ elco_irms_correct_isotopes <- function(x,
     y
   })
 
-  # exclude standards with too few measurements
-  x_standards_signature_median <- purrr::map(x_standards_signature_median, function(y) {
-    y[y$count >= t, , drop = FALSE]
-  })
-
-  # assign the isotope signature to substract from each measured value
-  x <- purrr::map2(x, x_standards_signature_median, function(y, z) {
-    res <-
-      purrr::map_df(seq_len(nrow(y)), function(i) {
-        index <- which.min(abs(y[i , as.character(disotope_area), drop = TRUE] - z$area_measured))
-        tibble::tibble(
-          standard_name = z$sample_label[[index]],
-          signature_measured = z$signature_measured[[index]],
-          signature_known = z$disotope[[index]],
-          area_measured = z$area_measured[[index]],
-          signature_difference = z$signature_difference[[index]]
-        )
-      })
-    dplyr::bind_cols(y, res)
+  # print warning if there are too few measurements in any file
+  cond <- purrr::map2_lgl(x_standards_signature_median, seq_along(x_standards_signature_median), function(y, i) {
+    cond <- !y$count >= t
+    if(cond) rlang::warn(paste0("For file ", i, ", there are less than ", t, " measurements for the selected reference standard."))
+    cond
   })
 
   # substract
-  x <- purrr::map(x, function(y) {
-    y[, as.character(disotope)] <- y[, as.character(disotope), drop = TRUE] - y$signature_difference
+  x <- purrr::map2(x, x_standards_signature_median, function(y, z) {
+    y[, as.character(disotope)] <- y[, as.character(disotope), drop = TRUE] - z$signature_difference
     y
   })
+
+  # check if the signature of any standards in `check` deviates too much from the median corrected signature of the standards
+  if(!is.null(check)) {
+    x_standards <- purrr::map(x, elco_irms_extract_standards)
+    x_standards_signature_median <- purrr::map(x_standards, function(y) {
+      y <- dplyr::group_by(y, sample_label)
+      y <- dplyr::summarise(y,
+                            file_id = unique(file_id),
+                            signature_measured = median(!!disotope),
+                            .groups = "keep")
+      y <- dplyr::left_join(y, check, by = "sample_label")
+      y$signature_difference <- y$signature_measured - y[, as.character(disotope), drop = TRUE]
+      # check thresholds
+      for(i in seq_len(nrow(y))) {
+        if(is.na(y[i, paste0("threshold_", disotope)])) {
+          rlang::warn(paste0("For standard ", y$sample_label[[i]], " isotope correction cannot be checked because the threshold given in `check` is `NA`."))
+        } else {
+          if(abs(y$signature_difference[[i]]) > y[i, paste0("threshold_", disotope)]) {
+            rlang::warn(paste0("For file ", y$file_id[[i]], " and standard ", y$sample_label[[i]], " the corrected isotope signature is larger than the reference threshold value."))
+          }
+        }
+      }
+      y
+    })
+  }
 
   # bind rows
   x <- dplyr::bind_rows(x)
 
   # plot
   if(plotit) {
+
+    # format for plotting
     x_or <-
       dplyr::bind_rows(
         dplyr::mutate(x_or, Corrected = FALSE),
         dplyr::mutate(x, Corrected = TRUE)
       )
-    x_or$sample_label <- ifelse(x_or$sample_label %in% irms_standards_sel$sample_label, x_or$sample_label, "Sample")
-    x_or$signature_known[x_or$sample_label == "Sample"] <- NA_real_
+    x_or$sample_label <- ifelse(x_or$sample_label %in% irms_standards$standard_name, x_or$sample_label, ifelse(x_or$sample_label == "bla", "Blank", "Sample"))
+    x_or$signature_known[x_or$sample_label %in% c("Sample", "Blank")] <- NA_real_
 
-    # plot without corrected samples (these often have differing values which makes it difficult to check the correction for the standards)
+    # plot (don't show blanks and samples because these often have such small values that checking for standards and samples gets impossible)
     print(
-      ggplot(dplyr::filter(x_or, sample_label != "Sample"), aes(x = sample_label)) +
-        geom_jitter(aes(y = !!disotope, colour = Corrected, shape = standard_name)) +
+      ggplot(dplyr::filter(x_or[x_or$sample_label != "Blank", ], sample_label != "Sample"), aes(x = sample_label)) +
+        geom_jitter(aes(y = !!disotope, colour = Corrected)) +
+        geom_point(data = irms_standards, aes(x = standard_name, y = !!disotope)) +
         stat_boxplot(aes(y = !!disotope, colour = Corrected), fill = NA) +
         geom_point(aes(x = sample_label, y = signature_known)) +
-        guides(shape = guide_legend(title = "Standard used for correction", nrow = 2),
-               colour = guide_legend(nrow = 2)) +
+        guides(colour = guide_legend(nrow = 2)) +
         theme(legend.position = "bottom") +
         labs(x = "Standards",
              y = bquote(delta^{.(stringr::str_extract(isotope, "[1-9]+"))}*.(stringr::str_extract(isotope, "[:alpha:]"))),
              title = "Corrected and uncorrected isotope signatures")
     )
+    # same plot as before, but now also with samples
     print(
-      ggplot(x_or, aes(x = sample_label)) +
-        geom_jitter(aes(y = !!disotope, colour = Corrected, shape = standard_name)) +
+      ggplot(x_or[x_or$sample_label != "Blank", ], aes(x = sample_label)) +
+        geom_jitter(aes(y = !!disotope, colour = Corrected)) +
         stat_boxplot(aes(y = !!disotope, colour = Corrected), fill = NA) +
         geom_point(aes(x = sample_label, y = signature_known)) +
-        guides(shape = guide_legend(title = "Standard used for correction", nrow = 2),
-               colour = guide_legend(nrow = 2)) +
+        guides(colour = guide_legend(nrow = 2)) +
         theme(legend.position = "bottom") +
         labs(x = "Standards and Samples",
              y = bquote(delta^{.(stringr::str_extract(isotope, "[1-9]+"))}*.(stringr::str_extract(isotope, "[:alpha:]"))),
@@ -175,7 +233,6 @@ elco_irms_correct_isotopes <- function(x,
 
   }
 
-  # discard unneded columns
-  x[, 1:14]
+  x
 
 }
