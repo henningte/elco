c6a5fa62d39cf15538f2067232e9c9febd450014
henningte <34153309+henningte@users.noreply.github.com>
2020-10-27 14:09:10 +0100

    Added standards and updated the standard label for BBOT to get a uniform format.

--- data-raw/irms_standards.R
+++ data-raw/irms_standards.R
@@ -10,17 +10,17 @@ m_caffeine_S <- units::set_units(0 * PeriodicTable::mass("S"), "g/mol")
 
 irms_standards <-
   tibble::tibble(
-    standard_name = c("IAEA 600", "BBOT_CN.", "BLS", "WFS"),
-    C = c(m_caffeine_C/m_caffeine, units::set_units(c(0.72, 0.4809, 0.398), "g/g")),
-    N =c( m_caffeine_N/m_caffeine, units::set_units(c(0.064, 0.0212, 0.0136), "g/g")),
-    S = c(m_caffeine_S/m_caffeine, units::set_units(c(0.0744, 0.00172, 0.00092), "g/g")),
-    H = c(m_caffeine_H/m_caffeine, units::set_units(c(NA, NA, NA), "g/g")),
-    O = c(m_caffeine_O/m_caffeine, units::set_units(c(NA, NA, NA), "g/g")),
-    "13C" = c(-27.771, -25.8, -27.2, -27.21),
-    "15N" = c(1, -0.6, -1.7, 2.85),
-    threshold_13C = c(0.2, NA, NA, NA),
-    threshold_15N = c(0.2 * 2, NA, NA, NA),
-    source = c("Coplen.2006", "IVAAnalysentechnikGmbH&Co.KGt.2016", NA, "IVAAnalysentechnikGmbH&Co.KGt.2016")
+    standard_name = c("IAEA 600", "BBOT", "BLS", "WFS", "SFS", "OAS_high", "OAS_low"),
+    C = c(m_caffeine_C/m_caffeine, units::set_units(c(0.72, 0.4809, 0.398, 0.4126, 0.0915, 0.0161), "g/g")),
+    N = c( m_caffeine_N/m_caffeine, units::set_units(c(0.064, 0.0212, 0.0136, 0.0147, 0.0062, 0.00133), "g/g")),
+    S = c(m_caffeine_S/m_caffeine, units::set_units(c(0.0744, 0.00172, 0.00092, 0.00095, 0.0069, 0.00014), "g/g")),
+    H = c(m_caffeine_H/m_caffeine, units::set_units(c(NA, NA, NA, NA, NA, NA), "g/g")),
+    O = c(m_caffeine_O/m_caffeine, units::set_units(c(NA, NA, NA, NA, NA, NA), "g/g")),
+    "13C" = c(-27.771, -25.8, -27.2, -27.21, -13.68, -26.27, -26.66),
+    "15N" = c(1, -0.6, -1.7, 2.85, 1.58, 4.42, 7.3),
+    threshold_13C = c(0.2, NA, NA, NA, NA, NA, NA),
+    threshold_15N = c(0.2 * 2, NA, NA, NA, NA, NA, NA),
+    source = c("Coplen.2006", "IVAAnalysentechnikGmbH&Co.KGt.2016", NA, "IVAAnalysentechnikGmbH&Co.KGt.2016", "IVAAnalysentechnikGmbH&Co.KGt.2016", "IVAAnalysentechnikGmbH&Co.KGt.2016", "IVAAnalysentechnikGmbH&Co.KGt.2016")
   )
 
 usethis::use_data(irms_standards, overwrite = TRUE)
